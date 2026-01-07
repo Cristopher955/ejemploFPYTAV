0599d72262317c5028a0665a3314d44a8b7fdd97
Servicios Digitales <Cetecom@SJAPCA03LC0810.duoc.acad>
2026-01-07 20:13:00 -0300

    mi primer problema git

---
+++ git 2.0.py
@@ -0,0 +1,13 @@
+edad = 22
+if edad > 54:
+    print("puede ver la pelicula con maximo descuento")
+elif edad >54:
+    print("puede ver la pelicula con descuentos")    
+elif edad > 17:
+    print("puedes ver la pelicula")
+#else:
+  #  print("no puedes entrar")
+   # print("ve a otro lado")
+     
+print("listo")
+    
